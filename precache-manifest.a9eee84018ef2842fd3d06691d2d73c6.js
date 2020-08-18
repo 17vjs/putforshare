128e062a38597a153221886a40506597ec0afbee
17vjs <36515090+17vjs@users.noreply.github.com>
2020-08-18 18:23:53 +0530

    Updates

---
+++ precache-manifest.a9eee84018ef2842fd3d06691d2d73c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af745e38f6fd930f151e57dc7f3d25b",
+    "url": "/putforshare/index.html"
+  },
+  {
+    "revision": "9a38377a7f50133762cc",
+    "url": "/putforshare/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "b8c2938ead1607454752",
+    "url": "/putforshare/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a38377a7f50133762cc",
+    "url": "/putforshare/static/js/2.e1ff9e00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/putforshare/static/js/2.e1ff9e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c2938ead1607454752",
+    "url": "/putforshare/static/js/main.8f228652.chunk.js"
+  },
+  {
+    "revision": "cc7b80161c7898168c13",
+    "url": "/putforshare/static/js/runtime-main.564be7aa.js"
+  }
+]);
